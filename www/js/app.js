cd4d2089c8438efa318fd29730d49c07ff14205e
rmcoombe <rosscoombe@gmail.com>
2018-04-01 13:21:13 +0100

    dfdv
    dsfvsdv

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,5 @@
+//----------Backendless Initialisation---------------------
+Backendless.initApp("AFBC8D7A-8A11-C898-FF89-DA475B8FF000","14C4D648-AF3B-0BE7-FF5C-29E1B2392E00");
 $(document).ready(function(){
 
 	var video = document.getElementById("video");
@@ -158,6 +160,8 @@ $(document).ready(function(){
 		if($("#image-area").is(":hidden")){
 
 			$("#image-area").fadeIn();
+			
+			updatePosition()
 
 			$(this).text("Hide Media Library");
 
@@ -166,6 +170,8 @@ $(document).ready(function(){
 				$("#image-area").append("<div id='no-img' class='error-msg'></div>");
 
 				$("#no-img").text("No images to display..");
+				
+				
 
 			} else{
 
@@ -188,3 +194,129 @@ $(document).ready(function(){
 	});
 
 });
+	$("#createGeoPoint").on('click', function(){
+		createGeoPoint()
+	
+	});
+
+
+
+
+
+
+
+
+
+
+
+//----------Backendless Initialisation---------------------
+Backendless.initApp("AFBC8D7A-8A11-C898-FF89-DA475B8FF000","14C4D648-AF3B-0BE7-FF5C-29E1B2392E00");
+
+
+//--------------Location Functions---------------------------------
+//when the jQuery Mobile page is initialised
+var watchID;
+
+var locationOptions = { 
+	maximumAge: 10000, 
+	timeout: 6000, 
+	enableHighAccuracy: true 
+};
+
+//when the jQuery Mobile page is initialised
+$(document).on('pageinit', function() {
+	
+	//set up listener for button clicks
+	//$('#startLocationButton').on('click', updatePosition);
+	//$('#stopLocationButton').on('click', stopPosition);
+	$('#createGeoPoint').on('click', createGeoPoint);
+	$('#uploadPhoto').on('click', uploadPhoto);
+	$('#moveFile').on('click', moveFile);
+	//change time box to show message
+	$('#time').val("Press the button to get location data");
+	
+});
+
+
+
+//Call this function when you want to watch for chnages in position
+function updatePosition() {
+	
+	//change time box to show updated message
+	$('#time').val("Getting data...");
+	
+	//instruct location service to get position with appropriate callbacks
+	watchID = navigator.geolocation.watchPosition(successPosition, failPosition, locationOptions);
+}
+
+//Call this function when you want to watch for chnages in position
+function stopPosition() {
+	
+	//change time box to show updated message
+	$('#time').val("Press the button to get location data");
+	
+	//instruct location service to get position with appropriate callbacks
+	navigator.geolocation.clearWatch(watchID);
+}
+
+
+//called when the position is successfully determined
+function successPosition(position) {
+	
+	//You can find out more details about what the position obejct contains here:
+	// http://www.w3schools.com/html/html5_geolocation.asp
+	
+
+	//lets get some stuff out of the position object
+	var time = position.timestamp;
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	
+	//OK. Now we want to update the display with the correct values
+	$('#time').val("Recieved data at " + time);
+	$('#lattext').val(latitude);
+	$('#longtext').val(longitude);
+	
+	//localStorage.setItem("time", unixtime);
+	localStorage.setItem("lattext", latitude);
+	localStorage.setItem("longtext", longitude);
+	
+	
+}
+
+//called if the position is not obtained correctly
+function failPosition(error) {
+	//change time box to show updated message
+	$('#time').val("Error getting data: " + error);
+	
+}
+
+function createGeoPoint() {
+	//alert(localStorage.lattext)
+	var latnum = parseFloat(localStorage.lattext);
+	var longnum = parseFloat(localStorage.longtext);
+	var geoEntry = document.getElementById("entry").value;
+	
+	var point = {
+		latitude: latnum,
+		longitude: longnum,
+		categories: ["My_Journal"],
+		metadata: {"Entry":geoEntry}
+	}
+	console.log(point);
+	
+	Backendless.Geo.addPoint( point )
+	.then( function( savedGeoPoint ) {
+	console.log( "geo point saved " + savedGeoPoint.geopoint.objectId );
+	})
+	.catch( function( error ) {
+	console.log( "error - " + error.message );
+	});
+}
+
+
+//------------End of Location Functions---------------------------------
+
+function test() {
+	alert(base64);
+}
